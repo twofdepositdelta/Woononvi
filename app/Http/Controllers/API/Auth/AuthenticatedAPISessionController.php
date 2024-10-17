20a42bf22e00d1eb309223745df6fe194d4c9a57
unknown <twoftechnologies@gmail.com>
2024-10-17 12:58:03 +0100

    API integrated

--- app/Http/Controllers/API/Auth/AuthenticatedAPISessionController.php
+++ app/Http/Controllers/API/Auth/AuthenticatedAPISessionController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
-class AuthenticatedSessionController extends Controller
+class AuthenticatedAPISessionController extends Controller
 {
     /**
      * Handle user authentication and return a JSON response.
