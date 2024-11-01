f8326db77fed072daacf30681c330660e1758fab
unknown <twoftechnologies@gmail.com>
2024-11-01 11:15:53 +0100

    Mises à jour

--- app/Http/Middleware/StepBasedMiddleware.php
+++ app/Http/Middleware/StepBasedMiddleware.php
@@ -18,7 +18,7 @@ class StepBasedMiddleware
     {
         if ($request->input('step') == 2) {
             // Si step est 2, vérifiez que l'utilisateur est authentifié
-            if (!Auth::guard('api')->check()) {
+            if (!Auth::guard('auth:sanctum')->check()) {
                 return response()->json(['message' => 'Non autorisé'], 401);
             }
         }
