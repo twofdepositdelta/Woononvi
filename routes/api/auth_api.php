ddf7ec6245a3e893185b809a50897572e511a53b
unknown <twoftechnologies@gmail.com>
2024-11-04 13:41:33 +0100

    Mises à jour

--- routes/api/auth_api.php
+++ routes/api/auth_api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\API\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\API\Auth\PasswordController;
 use App\Http\Controllers\API\ProfileController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\API\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
