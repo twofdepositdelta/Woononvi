707524234db5b54eaeecfefe211406a695d1630a
unknown <twoftechnologies@gmail.com>
2024-11-04 13:43:22 +0100

    Mises à jour

--- app/Http/Controllers/API/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/API/Auth/PasswordResetLinkController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+USE Carbon\Carbon;
 
 class PasswordResetLinkController extends Controller
 {
