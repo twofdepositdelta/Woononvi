06fb5ee565d1e9bd21d340523362ac0df6b6eb61
unknown <twoftechnologies@gmail.com>
2024-11-04 12:47:08 +0100

    Mises à jour

--- routes/api/auth_api.php
+++ routes/api/auth_api.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\API\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\API\Auth\PasswordController;
 use App\Http\Controllers\API\ProfileController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\PasswordController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\StepBasedMiddleware;
 
@@ -22,6 +22,7 @@ Route::middleware('guest:api')->group(function () {
 Route::middleware('auth:api')->group(function () {
     Route::put('finalise', [AuthenticatedSessionController::class, 'finalise'])->name('api.finalise');
     Route::put('profile', [ProfileController::class, 'update'])->name('api.profile.update');
+    Route::put('change-password', [PasswordController::class, 'update'])->name('api.password.update');
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware('throttle:6,1')->name('api.verification.send');
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store'])->name('api.password.confirm');
     Route::put('password', [PasswordController::class, 'update'])->name('api.password.update');
