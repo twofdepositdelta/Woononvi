4233acfad90923a23b7401c047902b14dac69ad7
unknown <twoftechnologies@gmail.com>
2024-11-06 16:50:47 +0100

    Message

--- app/Http/Controllers/API/ConversationController.php
+++ app/Http/Controllers/API/ConversationController.php
@@ -93,7 +93,7 @@ class ConversationController extends Controller
     private function assignSupportToConversation() {
         // Récupérer les supports actifs avec leur nombre de conversations non clôturées
         // $users = User::role('support')->get();
-        return User::role('support')->get()
+        return User::role('driver')->get()
         // ->filter(function ($user) {
         //     return $user->hasRole('support'); // Vérifie le rôle de support
         // })
