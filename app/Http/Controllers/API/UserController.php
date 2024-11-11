bbf06c70c52603ba3623347946d2ad5115f4b4d4
unknown <twoftechnologies@gmail.com>
2024-11-11 19:15:22 +0100

    Mises à jour

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
