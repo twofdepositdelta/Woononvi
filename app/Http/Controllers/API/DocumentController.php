760b3e2a64ec7238a4ba8b3886a562dad88bdb4a
unknown <twoftechnologies@gmail.com>
2024-11-12 18:22:43 +0100

    Mises à jour

--- app/Http/Controllers/API/DocumentController.php
+++ app/Http/Controllers/API/DocumentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Models\Vehicle;
 use App\Models\Document;
+use App\Models\TypeDocument;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
