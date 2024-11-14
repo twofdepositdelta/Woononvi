a6c9528d071993f821c0e0496fca81ae43f403b3
unknown <twoftechnologies@gmail.com>
2024-11-14 11:23:14 +0100

    Message

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -44,36 +44,15 @@ class PaymentController extends Controller
         $shop = $request->input('shop');
         $description = $request->input('description');
 
-        // Préparer les données pour la requête
-        // $data = [
-        //     'token' => $token,
-        //     'phoneNumber' => $phoneNumber,
-        //     'amount' => 1,
-        //     'shop' => $shop,
-        //     'description' => $description,
-        // ];
-
-        // Construire l'URL avec le mode
-        // $url = "https://api.feexpay.me/api/transactions/public/requesttopay/{$mode}";
-
         $response = Http::withHeaders([
             'Authorization' => "Bearer $token",
             'Content-Type' => 'application/json'
         ])->post("https://api.feexpay.me/api/transactions/public/requesttopay/{$mode}", [
             'shop' => $shop,
-            'amount' => $amount,
+            'amount' => 1,
             'phoneNumber' => $phoneNumber,
             'description' => $description,
         ]);
-
-        // $response = Http::withOptions([
-        //     'curl' => [
-        //         CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2, // Force l'utilisation de TLS 1.2
-        //     ],
-        // ])->post($url, $data);
-
-        // Envoyer la requête POST avec les données
-        // $response = Http::post($url, $data);
 
         // Vérifier la réponse de l'API
         if ($response->successful()) {
