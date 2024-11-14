c654fa0d19d069b7e26bdf88c51940f7c166205c
unknown <twoftechnologies@gmail.com>
2024-11-14 14:22:32 +0100

    DB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,7 +42,6 @@ class DatabaseSeeder extends Seeder
             ComissionSeeder::class,
             ApiSeeder::class,
             PaymentTypeSeeder::class,
-            PaymentSeeder::class,
         ]);
     }
 
