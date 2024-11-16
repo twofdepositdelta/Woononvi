a246e6ddb63d3eca8ef25b1731ecbaa8dea84082
TwoFTech <twoftechnologies@gmail.com>
2024-11-16 13:12:32 +0100

    header footer site

--- resources/views/front/layouts/partials/footer.blade.php
+++ resources/views/front/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer-area">
     <div class="footer-shape">
-        <img src="assets/img/shape/shape-8.png" alt>
+        <img src="{{ asset(FrontHelper::getEnvFolder() . 'storage/front/assets/img/shape/shape-8.png') }}" alt>
     </div>
     <div class="footer-widget">
         <div class="container">
@@ -8,46 +8,41 @@
                 <div class="col-md-6 col-lg-4">
                     <div class="footer-widget-box about-us">
                         <a href="#" class="footer-logo">
-                            <img src="assets/img/logo/logo-light.png" alt>
+                            <img src="{{ asset(FrontHelper::getEnvFolder() . 'storage/front/assets/img/logo/logo-light.png') }}" alt>
                         </a>
                         <p class="mb-3">
-                            We are many variations of passages available but the majority have suffered alteration
-                            in some form by injected humour words believable.
+                            Wononvi, la plateforme de covoiturage qui favorise la fraternité, la sécurité et l'accessibilité pour tous.
                         </p>
                         <ul class="footer-contact">
-                            <li><a href="tel:+21236547898"><i class="far fa-phone"></i>+2 123 654 7898</a></li>
-                            <li><i class="far fa-map-marker-alt"></i>25/B Milford Road, New York</li>
-                            <li><a
-                                    href="https://live.themewild.com/cdn-cgi/l/email-protection#84edeae2ebc4e1fce5e9f4e8e1aae7ebe9"><i
-                                        class="far fa-envelope"></i><span class="__cf_email__"
-                                        data-cfemail="9ef7f0f8f1defbe6fff3eef2fbb0fdf1f3">[email&#160;protected]</span></a>
-                            </li>
+                            <li><a href="tel:+22912345678"><i class="far fa-phone"></i>+229 12 34 56 78</a></li>
+                            <li><i class="far fa-map-marker-alt"></i>Cotonou, Bénin</li>
+                            <li><a href="mailto:contact@wononvi.com"><i class="far fa-envelope"></i> contact@wononvi.com</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-2">
                     <div class="footer-widget-box list">
-                        <h4 class="footer-widget-title">Quick Links</h4>
+                        <h4 class="footer-widget-title">Liens Rapides</h4>
                         <ul class="footer-list">
-                            <li><a href="#"><i class="fas fa-caret-right"></i> About Us</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Update News</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Testimonials</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Terms Of Service</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Privacy policy</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Our Drivers</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> À Propos de Nous</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Actualités</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Témoignages</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Conditions d'utilisation</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Politique de confidentialité</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Nos Conducteurs</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="footer-widget-box list">
-                        <h4 class="footer-widget-title">Support Center</h4>
+                        <h4 class="footer-widget-title">Centre de Support</h4>
                         <ul class="footer-list">
-                            <li><a href="#"><i class="fas fa-caret-right"></i> FAQ's</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Affiliates</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Booking Tips</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Book A Ride</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Contact Us</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Sitemap</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> FAQ</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Affiliation</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Conseils de réservation</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Réserver un trajet</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Contactez-nous</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Plan du site</a></li>
                         </ul>
                     </div>
                 </div>
@@ -55,12 +50,12 @@
                     <div class="footer-widget-box list">
                         <h4 class="footer-widget-title">Newsletter</h4>
                         <div class="footer-newsletter">
-                            <p>Subscribe Our Newsletter To Get Latest Update And News</p>
+                            <p>Abonnez-vous pour recevoir les dernières actualités et offres de Wononvi.</p>
                             <div class="subscribe-form">
                                 <form action="#">
-                                    <input type="email" class="form-control" placeholder="Your Email">
+                                    <input type="email" class="form-control" placeholder="Votre Email">
                                     <button class="theme-btn" type="submit">
-                                        Subscribe Now <i class="far fa-paper-plane"></i>
+                                        S'abonner <i class="far fa-paper-plane"></i>
                                     </button>
                                 </form>
                             </div>
@@ -75,7 +70,8 @@
             <div class="row">
                 <div class="col-md-6 align-self-center">
                     <p class="copyright-text">
-                        &copy; Copyright <span id="date"></span> <a href="#"> TAXIO </a> All Rights Reserved.
+                        &copy; Copyright {{ date('Y') }} <a href="{{ route('index') }}">Wononvi</a> Tous droits réservés.
+                        Développé par <a href="https://twoftechnologies.com/" target="_blank"><span style="color: orange;">TwoF</span> <span style="color: skyblue;">Technologies</span></a>.
                     </p>
                 </div>
                 <div class="col-md-6 align-self-center">
