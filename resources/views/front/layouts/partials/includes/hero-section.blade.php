0e3a30fbd439519c670dabdca7abab8c3fa34d81
TwoFTech <twoftechnologies@gmail.com>
2024-11-16 12:08:00 +0100

    all

--- resources/views/front/layouts/partials/includes/hero-section.blade.php
+++ resources/views/front/layouts/partials/includes/hero-section.blade.php
@@ -1,33 +1,37 @@
-<div class="hero-section">
-    <div class="hero-single">
-        <div class="hero-shape">
-            <img src="{{ asset(FrontHelper::getEnvFolder() . 'storage/front/assets/img/shape/shape-9.png') }}" alt>
-        </div>
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-12 col-lg-6">
-                    <div class="hero-content">
-                        <h6 class="hero-sub-title wow fadeInUp" data-wow-delay=".25s">Welcome To
-                            Taxio!</h6>
-                        <h1 class="hero-title wow fadeInRight" data-wow-delay=".50s">
-                            Book <span>Taxi</span> For Your Ride
-                        </h1>
-                        <p class="wow fadeInLeft" data-wow-delay=".75s">
-                            There are many variations of passages available the majority have suffered
-                            alteration in some form generators on the Internet tend to repeat predefined
-                            chunks injected humour randomised words look even slightly believable.
-                        </p>
-                        <div class="hero-btn wow fadeInUp" data-wow-delay="1s">
-                            <a href="#" class="theme-btn">About More<i class="fas fa-arrow-right"></i></a>
-                            <a href="#" class="theme-btn theme-btn2">Learn More<i
-                                    class="fas fa-arrow-right"></i></a>
-                        </div>
+<div class="about-area py-120">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="about-left wow fadeInLeft" data-wow-delay=".25s">
+                    <div class="about-img">
+                        <img src="{{ asset(FrontHelper::getEnvFolder() . 'storage/front/assets/img/about/01.png') }}" alt>
                     </div>
                 </div>
-                <div class="col-md-12 col-lg-6">
-                    <div class="hero-img wow fadeInRight" data-wow-delay=".25s">
-                        <img src="{{ asset(FrontHelper::getEnvFolder() . 'storage/front/assets/img/slider/01.jpg') }}" alt>
+            </div>
+            <div class="col-lg-6">
+                <div class="about-right wow fadeInRight" data-wow-delay=".25s">
+                    <div class="site-heading mb-3">
+                        <h2 class="site-title">
+                            Simplifiez vos trajets avec <span>Wononvi</span>
+                        </h2>
                     </div>
+                    <p class="about-text">
+                        Profitez d'un covoiturage sécurisé et abordable. Wononvi connecte ceux qui veulent partager leur trajet et voyager en toute sérénité.
+                    </p>
+                    <div class="about-list-wrapper">
+                        <ul class="about-list list-unstyled">
+                            <li>
+                                <strong>Pour les conducteurs</strong> : Partagez vos trajets et économisez sur vos déplacements.
+                            </li>
+                            <li>
+                                <strong>Pour les passagers</strong> : Trouvez facilement un trajet, économisez et voyagez confortablement.
+                            </li>
+                            <li>
+                                Rejoignez une communauté conviviale et bienveillante.
+                            </li>
+                        </ul>
+                    </div>
+                    <a href="#" class="theme-btn mt-4">En savoir plus<i class="fas fa-arrow-right"></i></a>
                 </div>
             </div>
         </div>
