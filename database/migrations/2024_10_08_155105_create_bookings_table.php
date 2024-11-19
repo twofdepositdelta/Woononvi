d273d478fa844f1ef1fbb747fc11329c6a43d353
unknown <twoftechnologies@gmail.com>
2024-11-19 10:18:39 +0100

    Mises à jour

--- database/migrations/2024_10_08_155105_create_bookings_table.php
+++ database/migrations/2024_10_08_155105_create_bookings_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Ride;
-use App\Models\Trip;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -22,12 +21,12 @@ return new class extends Migration
             $table->foreignIdFor(Ride::class);
             $table->foreignId('passenger_id')->constrained('users')->onDelete('cascade');
             $table->enum('status', ['pending', 'accepted', 'rejected', 'validated_by_passenger', 'validated_by_driver', 'refunded', 'cancelled'])->default('pending');
-            $table->timestamps('accepted_at');
-            $table->timestamps('rejected_at');
-            $table->timestamps('validated_by_passenger_at');
-            $table->timestamps('validated_by_driver_at');
-            $table->timestamps('refunded_at');
-            $table->timestamps('cancelled_at');
+            $table->timestamp('accepted_at')->nullable();
+            $table->timestamp('rejected_at')->nullable();
+            $table->timestamp('validated_by_passenger_at')->nullable();
+            $table->timestamp('validated_by_driver_at')->nullable();
+            $table->timestamp('refunded_at')->nullable();
+            $table->timestamp('cancelled_at')->nullable();
             $table->timestamps();
         });
     }
