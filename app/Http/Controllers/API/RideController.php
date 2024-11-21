577d802219bf40e889443ffb1d63d736ad107f4f
unknown <twoftechnologies@gmail.com>
2024-11-21 12:11:10 +0100

    Mises à jour

--- app/Http/Controllers/API/RideController.php
+++ app/Http/Controllers/API/RideController.php
@@ -213,10 +213,11 @@ class RideController extends Controller
 
         $rides = DB::table('rides')
     ->select('id', 'start_location', DB::raw("
-        ST_Distance_Sphere(start_location, ST_GeomFromText('POINT(" . $request->start_lng . " " . $request->start_lat . ")', 4326)) AS distance
-    "))
-    // ->having('distance', '<=', $radius)
+        ST_Distance_Sphere(start_location, ST_GeomFromText('POINT(? ?)')) AS distance
+    "), [$request->start_lng, $request->start_lat]) // Paramètres liés
+    // ->having('distance', '<=', $radius) // Vous pouvez ajouter cette condition si nécessaire
     ->get();
+
 return $rides;
         // Retourner les trajets qui correspondent
         return response()->json([
