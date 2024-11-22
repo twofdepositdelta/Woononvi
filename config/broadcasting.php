1c9ae7c31351e3fcaebeb880dc7c0cdf1438e186
unknown <twoftechnologies@gmail.com>
2024-11-22 17:33:00 +0100

    Mises à jour

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -12,9 +12,8 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'cluster' => env('PUSHER_CLUSTER'),
+                'useTLS' => true,
                 'port' => env('PUSHER_PORT', 6001),
                 'scheme' => env('PUSHER_SCHEME', 'http'),
                 'encrypted' => env('PUSHER_ENCRYPTED', true),
