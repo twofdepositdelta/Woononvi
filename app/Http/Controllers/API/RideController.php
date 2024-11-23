15455ac9062fb1e524ef26f080df98672a88e8c1
unknown <twoftechnologies@gmail.com>
2024-11-23 11:11:02 +0100

    Mises à jour

--- app/Http/Controllers/API/RideController.php
+++ app/Http/Controllers/API/RideController.php
@@ -210,7 +210,7 @@ class RideController extends Controller
             'vehicles.licence_plate',
             'vehicles.vehicle_mark',
             'vehicles.vehicle_model',
-            DB::raw("CONCAT('" . asset() . "/', profiles.avatar) as avatar"),
+            DB::raw("CONCAT('" . asset('storage/avatars') . "/', profiles.avatar) as avatar"),
             'days',
             'type',
             'departure_time',
