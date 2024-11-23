9624fe550b3a9093272e2ff1ee5e0e05aa1d303d
unknown <twoftechnologies@gmail.com>
2024-11-23 13:37:45 +0100

    Mises à jour

--- app/Http/Controllers/API/RideController.php
+++ app/Http/Controllers/API/RideController.php
@@ -243,13 +243,13 @@ class RideController extends Controller
         //     'driver_id' => 'required|exists:users,id', // Vérifie que l'ID du conducteur existe
         // ]);
 
-        if ($validator->fails()) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Les données envoyées ne sont pas valides.',
-                'errors' => $validator->errors()->all(),
-            ], 422);
-        }
+        // if ($validator->fails()) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'Les données envoyées ne sont pas valides.',
+        //         'errors' => $validator->errors()->all(),
+        //     ], 422);
+        // }
 
         // Récupération des réservations liées au conducteur
         $pendingBookings = DB::table('bookings')
