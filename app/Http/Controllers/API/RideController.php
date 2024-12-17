ac2ce57d08586df2c4b69e3ab048f920b5bca05f
unknown <twoftechnologies2024@gmail.com>
2024-12-17 10:48:03 +0100

    Mises à jour

--- app/Http/Controllers/API/RideController.php
+++ app/Http/Controllers/API/RideController.php
@@ -516,7 +516,7 @@ class RideController extends Controller
             ->where('bookings.status', $request->status); // Filtrer par statut
 
         // Si le statut est 'in progress', récupérer uniquement la première réservation
-        if ($request->status === 'in progress' || $request->status === 'validated_by_driver') {
+        if ($request->status === 'in progress' || $request->status === 'validated_by_passenger') {
             $booking = $query->first();
 
             if (!$booking) {
