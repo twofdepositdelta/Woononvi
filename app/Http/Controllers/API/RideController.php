f333deaedfc0996b66f39a6717b297f2ed75f5e6
unknown <twoftechnologies2024@gmail.com>
2024-12-27 12:47:15 +0100

    Mises à jour

--- app/Http/Controllers/API/RideController.php
+++ app/Http/Controllers/API/RideController.php
@@ -115,6 +115,7 @@ class RideController extends Controller
      */
     public function store(Request $request)
     {
+        logger()->error('E', ['user_id' => $request]);
         // Validation des données
         $validator = Validator::make($request->all(), [
             'type' => 'required|in:Régulier,Ponctuel',
