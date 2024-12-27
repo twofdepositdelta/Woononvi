edae91b42a2e33751947b3190a1002317841f911
unknown <twoftechnologies2024@gmail.com>
2024-12-27 16:35:44 +0100

    Mises à jour

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -43,21 +43,6 @@ class Booking extends Model
         return $this->belongsTo(User::class, 'passenger_id');
     }
 
-    // protected $fillable = [
-    //     'seats_reserved',
-    //     'total_price',
-    //     'status',
-    //     'ride_id', // Clé étrangère pour le trajet
-    //     'passenger_id', // Clé étrangère pour le passager
-    //     'booking_number'
-    // ];
-
-    // Relation avec le trajet
-    // public function ride()
-    // {
-    //     return $this->belongsTo(Ride::class); // Assurez-vous d'importer le modèle Ride
-    // }
-
     public function payments()
     {
         return $this->hasMany(Payment::class, 'booking_id'); // Assurez-vous d'importer le modèle Booking
